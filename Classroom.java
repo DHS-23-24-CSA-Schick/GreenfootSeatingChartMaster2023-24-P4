0af4487774f5171651933a0eb6902d16d3988e79
unknown <joby8784@mydusd.org>
2023-09-01 11:18:26 -0700

    CyrilJoby

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        CyrilJoby cyriljoby = new CyrilJoby();
+        addObject(cyriljoby,5,3);
+        cyriljoby.assignSeat();
   
     }
     
