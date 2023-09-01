bd6da9cf3a8ceff66b7026b6bd562c9ffcae5a5f
Akshat Khandelwal <khandelwal7749@mydusd.org>
2023-09-01 11:48:39 -0700

    first commit

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        AkshatKhandelwal akshatkhandelwal = new AkshatKhandelwal();
+        addObject(akshatkhandelwal,6,3);
+        akshatkhandelwal.assignSeat();
   
     }
     
