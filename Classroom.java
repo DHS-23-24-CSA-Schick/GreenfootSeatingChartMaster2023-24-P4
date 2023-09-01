60620d0809d1d6da77d58fd5cae5ff01f1b52ba6
Bardia Anvari <anvari9494@mydusd.org>
2023-09-01 10:47:55 -0700

    changes

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        Bardia bardia = new Bardia();
+        addObject(bardia, 2, 7);
+        bardia.assignSeat();
   
     }
     
