23e111ce91992ec72a1ffb435c20f72f982a755a
KevinLi-1 <li9651@mydusd.org>
2023-09-03 16:59:00 -0700

    Programming file for Kevin Li and table group's method

--- StudentDeskGroup.java
+++ StudentDeskGroup.java
@@ -9,8 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * 
  * methods should be public, so they can be called when the scenario is in pause mode.
  * 
- * @author (your name (add your groups names here, and tie to any method that you add) 
- * @version (a version number or a date. Update this for each revision)
+ * @author Kevin, Peyton, Matthew, Arnav 
+ * @version 9/1/2023
  */
 public class StudentDeskGroup extends Actor
 {
@@ -21,5 +21,37 @@ public class StudentDeskGroup extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        
+    }   
+    public void displayTableNumber(int studentNumber){
+        //Returns the table number based on the seat number. If the seat number exceeds the existing number of desks,
+        //it will display that the seat number does not exist in the classroom
+        if (studentNumber <= 4){
+            System.out.print("This is table numnber 1.");
+        }
+        else if (studentNumber <= 8) {
+            System.out.print("This is table number 2."); 
+        }
+        else if (studentNumber <= 12) {
+            System.out.print("This is table number 3."); 
+        }
+        else if (studentNumber <= 16) {
+            System.out.print("This is table number 4."); 
+        }
+        else if (studentNumber <= 20) {
+            System.out.print("This is table number 5."); 
+        }
+        else if (studentNumber <= 24) {
+            System.out.print("This is table number 6."); 
+        }
+        else if (studentNumber <= 28) {
+            System.out.print("This is table number 7."); 
+        }
+        else if (studentNumber <= 32) {
+            System.out.print("This is table number 8."); 
+        }
+        else {
+            System.out.print("Student desk number " + studentNumber + "is not in this classroom.");
+        }
+    }
 }
