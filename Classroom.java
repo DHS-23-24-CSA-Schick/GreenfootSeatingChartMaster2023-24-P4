122048371584e09ac6e558fc1e8023e1e4470282
Aarnav Koushik <aarnavkoushik@Aarnavs-MBP.home.arpa>
2023-09-03 22:21:34 -0700

    Greenfoot

--- Classroom.java
+++ Classroom.java
@@ -149,8 +149,8 @@ public class Classroom extends World
  // Each student needs to create their specific instance following the KilgoreTrout example.
  // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
  // and then clicking on the inspect text
-        KilgoreTrout kilgoretrout = new KilgoreTrout();
-        addObject(kilgoretrout,2,3);
+        KoushikAarnav kilgoretrout = new KoushikAarnav();
+        addObject(kilgoretrout,6,6);
         kilgoretrout.assignSeat();
   
     }
