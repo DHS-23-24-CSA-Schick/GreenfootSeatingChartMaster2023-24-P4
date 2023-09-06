6903a0fe2702f4f9c672068425b6a97c5847d21d
RyanKim1245 <kim1311@mydusd.org>
2023-09-06 15:34:11 -0700

    adding classroom file

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        RyanKim ryankim = new RyanKim();
+        addObject(ryankim,6,4);
+        kilgoretrout.assignSeat();
   
     }
     
